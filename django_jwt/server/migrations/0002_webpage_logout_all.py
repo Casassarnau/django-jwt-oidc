007ff9e34425f28816dcd2396814a7c71064d048
Arnau Casas <33383702+Casassarnau@users.noreply.github.com>
2023-04-21 18:04:14 +0200

    Fixed migrations issue

--- django_jwt/server/migrations/0002_webpage_logout_all.py
+++ django_jwt/server/migrations/0002_webpage_logout_all.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('server', '0001_initial'),
+        ('django_jwt_server', '0001_initial'),
     ]
 
     operations = [
